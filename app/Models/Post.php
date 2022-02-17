c66eedab3f5bac294eb50ea752dd48797aee5a67
bagasysf <bagasysf@gmail.com>
2022-02-17 16:50:18 +0700

    your first eloquent relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
-    // protected $guarded = ['id'];
-    // protected $fillable = ['title','excerpt','body'];
-
-    // public function getRouteKeyName() {
-    //     return 'slug';
-    // }
+    
+    public function category() {
+        // hasOne, hasMany, belongsTo, belongsToMany
+        return $this->BelongsTo(Category::class);
+    }
 }
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,11 @@
                     {{ $post->title; }}
                 </a>
             </h1>
+            <p>
+                <a href="#">
+                    {{ $post->category->name; }}
+                </a>
+            </p>
             <p>{{ $post->excerpt; }}</p>
         </article>
     @endforeach
