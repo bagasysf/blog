7e33e8d0464f2a3e13adee4e6849afd24ef664e1
bagasysf <bagasysf@gmail.com>
2022-02-20 14:29:24 +0700

    database seeding saving times

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $hobbies = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'slug' => 'my-personal-post',
+            'title' => 'My Personal Post',
+            'excerpt' => 'Lorem, ipsum dolor.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fuga dicta voluptatum dolorem ullam inventore ut ratione adipisci nostrum rem nemo, nam ea quaerat! Harum, et eaque. Quia ut maiores minus dolorum? Tenetur dolor dolorem cum reprehenderit voluptate qui praesentium quas iusto quidem voluptates rerum quod, ipsa quis consequuntur, illo dolore inventore modi animi facilis non maxime fugiat, voluptatibus impedit unde. Quia minima dignissimos fugiat unde optio sapiente nostrum earum vel quidem nisi consequuntur ipsa mollitia, dolor exercitationem quasi nobis ipsum saepe quas magnam illum. Earum fugiat officia beatae rerum doloribus aliquam sit obcaecati provident, delectus error amet dicta tempore!'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'slug' => 'my-work-post',
+            'title' => 'My Work Post',
+            'excerpt' => 'Lorem, ipsum dolor.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fuga dicta voluptatum dolorem ullam inventore ut ratione adipisci nostrum rem nemo, nam ea quaerat! Harum, et eaque. Quia ut maiores minus dolorum? Tenetur dolor dolorem cum reprehenderit voluptate qui praesentium quas iusto quidem voluptates rerum quod, ipsa quis consequuntur, illo dolore inventore modi animi facilis non maxime fugiat, voluptatibus impedit unde. Quia minima dignissimos fugiat unde optio sapiente nostrum earum vel quidem nisi consequuntur ipsa mollitia, dolor exercitationem quasi nobis ipsum saepe quas magnam illum. Earum fugiat officia beatae rerum doloribus aliquam sit obcaecati provident, delectus error amet dicta tempore!'
+        ]);
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,9 @@
 <x-layout>
     <article>
         <h1>{{ $post->title; }}</h1>
-        <p>
-                <a href="/categories/{{ $post->category->slug }}">
-                    {{ $post->category->name; }}
-                </a>
+            <p>
+                By <a href="#">{{ $post->user->name }}</a>
+                in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name; }}</a>
             </p>
         <p>{{ $post->body; }}</p>
 
