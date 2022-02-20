bdc35e9d5fd672e4d63cd1e3b6f139435db4902f
bagasysf <bagasysf@gmail.com>
2022-02-20 17:06:34 +0700

    view all post by an author

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
@@ -17,12 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // \Illuminate\Support\Facades\DB::Listen(function ($query) {
-    //     logger($query->sql, $query->bindings);
-    // });
-
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->with('category', 'author')->get()
     ]);
 });
 
@@ -38,6 +35,11 @@ Route::get('categories/{category:slug}', function (Category $category) {
     ]);
 });
 
-
+Route::get('authors/{author:username}', function (User $author) {
+    // dd($author);
+    return view('posts', [
+        'posts' => $author->posts
+    ]);
+});
 
 
