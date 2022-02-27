26c205d6a0f167f54a30f0bded7c1efa9afcfa54
bagasysf <bagasysf@gmail.com>
2022-02-27 21:54:58 +0700

    author filtering

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -29,6 +29,12 @@ class Post extends Model
             fn ($query, $category) =>
             $query->whereHas('category', fn ($query) => $query->where('slug', $category))
         );
+
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas('author', fn ($query) => $query->where('username', $author))
+        );
     }
 
     public function category()
