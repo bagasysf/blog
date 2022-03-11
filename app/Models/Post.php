be6ec9c6f906e884426850a37b41003ead007f4f
bagasysf <bagasysf@gmail.com>
2022-03-11 18:35:27 +0700

    make section comment dynamic

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -39,6 +39,11 @@ class Post extends Model
         );
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function category()
     {
         // hasOne, hasMany, belongsTo, belongsToMany
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,17 +1,17 @@
+@props(['comment'])
 <article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/60" width="60" height="60" alt="" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-semibold">John Doe</h3>
+            <h3 class="font-semibold">{{ $comment->author->name }}</h3>
             <p class="text-xs">
                 Posted
-                <time>8 hours ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus aliquam,
-                asperiores blanditiis culpa eligendi fugit harum laborum, minus numquam obcaecati,
-                odit officia officiis quam qui quia ratione ut voluptatibus.
+            <p>
+                {{ $comment->body }}
             </p>
         </header>
     </div>
