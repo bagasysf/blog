cf641b05f9a5e9fb9521f5c64688667b041d0f09
bagasysf <bagasysf@gmail.com>
2022-03-13 05:53:21 +0700

    make a section comment dynamic part2

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,7 +1,7 @@
 @props(['comment'])
 <article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" width="60" height="60" alt="" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" width="60" height="60" alt="" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
