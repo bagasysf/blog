d7fc388ac14ecddc3c549eea76acfebb149d5433
bagasysf <bagasysf@gmail.com>
2022-03-24 07:25:22 +0700

    design the comment form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,6 +49,23 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-panel>
+                        <form method="POST" action="#">
+                            @csrf
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" width="40" height="40" alt="" class="rounded-full">
+
+                                <h2 class="ml-4">Want to participate?</h2>
+                            </header>
+                            <div class="mt-6">
+                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" id="" rows="5" placeholder="Quick, think of something!"></textarea>
+                            </div>
+                            <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
+                                <button class="bg-blue-500 py-2 px-10 uppercase text-white rounded-full text-xs hover:bg-blue-600">Post</button>
+                            </div>
+                        </form>
+                    </x-panel>
+
                     @foreach ($post->comments as $comment)
                     <x-post-comment :comment="$comment" />
                     @endforeach
